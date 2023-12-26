7acffc3f1f471bf708877eb4dad5efd2a0a98cbf
danieldiasm <daniel_diasm@hotmail.com>
2023-12-26 20:36:54 +0100

    added some basic code to PoC

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import time
-import traceback
-from PIL import Image, ImageDraw, ImageFont
+from time import sleep
 
-import oled.SH1106
-import oled.config
 from interface.ui import UI as UI
+
+display_ui = UI(splash_img="splash.bmp")
+
+display_ui.show_splash()
+sleep(10)
+display_ui.clear_screen()
