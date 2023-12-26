2550a9dbfaed861a4486675820d09fbd24a4651c
danieldiasm <daniel_diasm@hotmail.com>
2023-12-26 21:07:41 +0100

    Adding Display Init Failure error Raise

--- interface/ui.py
+++ interface/ui.py
@@ -5,7 +5,9 @@ class UI(object):
 
     def __init__(self, display:SH1106 = SH1106(), splash_img: str = None) -> None:
         def Initialize_Display():
-            self.display.Init()
+            result = self.display.Init()
+            if not result:
+                raise RuntimeError("Init Display Failed!")
             self.display.clear()
    
         self.display = display
