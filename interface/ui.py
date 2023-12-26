cf31cc1d52236e07fa020bc0ab5f28228b057c1f
danieldiasm <daniel_diasm@hotmail.com>
2023-12-26 20:36:25 +0100

    added basic ui code for PoC

--- interface/ui.py
+++ interface/ui.py
@@ -1,19 +1,34 @@
 from oled.SH1106 import SH1106
+from PIL import Image,ImageDraw,ImageFont
 
 class UI(object):
 
-    def __init__(self, display:SH1106, splash_img: str = None) -> None:
+    def __init__(self, display:SH1106 = SH1106(), splash_img: str = None) -> None:
+        def Initialize_Display():
+            if not self.display.Init():
+                return False
+            self.display.clear()
+            return True
+        
         self.display = display
         self.splash_img = splash_img
+        if not Initialize_Display():
+            raise RuntimeError("Failed to initialize display!")
 
-    def show_splash():
+    def show_splash(self):
+        splash_image = Image.new('1', (self.display.width, self.display.height), 255)
+        splash_bmp = ImageDraw.open(splash_image)
+        splash_image.paste(splash_bmp, (0,5))
+        self.display.ShowImage(self.display.getbuffer(splash_bmp))
+
+    def show_message(self):
         pass
 
-    def show_message():
+    def show_menu(self):
         pass
 
-    def show_menu():
-        pass
+    def clear_screen(self):
+        self.display.clear()
 
 class Menu(object):
 
