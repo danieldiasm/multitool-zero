3c3b584721b063254483d485d89e9319aa6a0aad
danieldiasm <daniel_diasm@hotmail.com>
2023-12-26 19:59:53 +0100

    added a button setup and control class

---
+++ interface/control.py
@@ -0,0 +1,50 @@
+
+import RPi.GPIO as GPIO
+
+class Buttons(object):
+
+    def __init__(self) -> None:
+
+        def hw_setup():
+            GPIO.setmode(GPIO.BCM)
+            for key, value in self.keymap.items():
+                GPIO.setup(value, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        self.keymap = {
+            "KEY_UP_PIN": 6 ,
+            "KEY_DOWN_PIN": 19,
+            "KEY_LEFT_PIN": 5,
+            "KEY_RIGHT_PIN": 26,
+            "KEY_CENTER_PIN": 13,
+            "KEY1_PIN": 21,
+            "KEY2_PIN": 20,
+            "KEY3_PIN": 16,
+        }
+        hw_setup()
+
+    def key_press(self, key):
+        GPIO.input(key)
+    
+    def key_up(self):
+        self.key_press(self.keymap["KEY_UP_PIN"])
+
+    def key_down(self):
+        self.key_press(self.keymap["KEY_DOWN_PIN"])
+            
+    def key_left(self):
+        self.key_press(self.keymap["KEY_LEFT_PIN"])
+
+    def key_right(self):
+        self.key_press(self.keymap["KEY_RIGHT_PIN"])
+
+    def key_center(self):
+        self.key_press(self.keymap["KEY_CENTER_PIN"])
+
+    def key_1(self):
+        self.key_press(self.keymap["KEY1_PIN"])
+
+    def key_2(self):
+        self.key_press(self.keymap["KEY2_PIN"])
+
+    def key_3(self):
+        self.key_press(self.keymap["KEY3_PIN"])
