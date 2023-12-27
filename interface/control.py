5557ab31919533da304d3e526d8b2d3d9d3d7fd9
danieldiasm <daniel_diasm@hotmail.com>
2023-12-27 09:16:09 +0100

    Adding ToDo and moving comments

--- interface/control.py
+++ interface/control.py
@@ -5,11 +5,12 @@ class Buttons(object):
 
     def __init__(self) -> None:
 
-        def hw_setup():
+        def control_hw_setup():
             GPIO.setmode(GPIO.BCM)
             for key, value in self.keymap.items():
                 GPIO.setup(value, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+        
+        # TODO Make this keymap come from an INI file
         self.keymap = {
             "KEY_UP_PIN": 6 ,
             "KEY_DOWN_PIN": 19,
@@ -20,7 +21,7 @@ class Buttons(object):
             "KEY2_PIN": 20,
             "KEY3_PIN": 16,
         }
-        hw_setup()
+        control_hw_setup()
 
     def key_press(self, key):
         GPIO.input(key)
